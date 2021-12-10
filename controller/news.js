a4868432db6f3852845b38873b77381f9f42beee
matthewngid20 <matthew.ng.id20@gmail.com>
2021-12-10 19:28:09 +1100

    Seperate files to sperate modules

---
+++ controller/news.js
@@ -0,0 +1,20 @@
+const News = require('../news/news')
+const imageProcess = require('../util/imageProcess')
+try {
+    const createNews = async (req, res) => {
+    
+        const news = new News()
+        const id = news.createId()
+        
+        const imageName = await imageProcess(req,id)
+    
+        news.create(req.body,id,imageName) 
+        res.send('submitted')
+    }
+} catch (error) {
+    console.log("error inside CreateNews", error.message);
+}
+
+module.exports = {
+    createNews
+}
--- routers/news.js
+++ routers/news.js
@@ -4,21 +4,12 @@ const multer = require('multer')
 const fs = require('fs')
 //image resize module
 const sharp = require('sharp')
-const imageProcess = require('../util/imageProcess')
-const storage = multer.memoryStorage()
-const uploads = multer({storage})
-const News = require('../news/news')
+
+const uploads = require('../middleware/multer')
+const { createNews } = require('../controller/news')
+
 
 //get id from news
-router.post('/create', uploads.single('thumbnail'), async (req, res) => {
-    
-    const news = new News()
-    const id = news.createId()
-    
-    const imageName = await imageProcess(req,id)
-
-    news.create(req.body,id,imageName) 
-    res.send('submitted')
-})
+router.post('/create', uploads.single('thumbnail'),createNews )
 
 module.exports = router
