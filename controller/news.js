ae4f5f169b3e0c9ac7157135f434bbdbb5be5a11
matthewngid20 <matthew.ng.id20@gmail.com>
2021-12-11 11:05:34 +1100

    created get single news router in controller

--- controller/news.js
+++ controller/news.js
@@ -5,26 +5,39 @@ const imageProcess = require('../util/imageProcess')
 const createNews = async (req, res) => {
     try {
         const id = news.createId()
-        
-        const imageName = await imageProcess(req,id)
-    
-        news.create(req.body,id,imageName) 
-        res.json({success: true, message: "successfully submitted"})
+
+        const imageName = await imageProcess(req, id)
+
+        news.create(req.body, id, imageName)
+        res.json({ success: true, message: "successfully submitted" })
     }
     catch (error) {
-        res.json({success: false, message: "failed to submit, server error"})
+        res.json({ success: false, message: "failed to submit, server error" })
         console.log("error inside CreateNews", error.message);
     }
-} 
-const getAllNews = async (req,res)=> {
+}
+const getAllNews = async (req, res) => {
     try {
         const data = await news.getAll()
-        res.json({success:true, news:data})
-    }catch (error){
-        console.log("error inside getAllNews controller",error.message);
+        res.json({ success: true, news: data })
+    } catch (error) {
+        console.log("error inside getAllNews controller", error.message);
+    }
+}
+const getSingleNews = async (req, res) => {
+    try {
+        const data = await news.getSingle(req.params.id)
+        if (!data) {
+            return res.json({ success: false, message: 'No post found!' })
+        } 
+        res.json({ success: true, news: data })
+    } catch (error) {
+        res.json({ success: false, news: data })
+        console.log("error inside getSingleNews controller", error.message);
     }
 }
 module.exports = {
     createNews,
-    getAllNews
+    getAllNews,
+    getSingleNews
 }
--- routers/news.js
+++ routers/news.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 //image resize module
 const uploads = require('../middleware/multer')
-const { createNews,getAllNews } = require('../controller/news')
+const { createNews,getAllNews,getSingleNews } = require('../controller/news')
 const {validator, result,validateFile} =require('../middleware/validator')
 
 //get id from news
 router.post('/create', uploads.single('thumbnail'),validator, validateFile,result,createNews )
 router.get('/news', getAllNews)
+router.get('/news/single/:id', getSingleNews)
 module.exports = router
